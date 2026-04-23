4176dbd12ad9c87d4d8f41191ba6872582e8efc0
karina <karinann514@gmail.com>
2026-04-23 15:53:28 -0400

    More formatting errors

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // For testing purposes, we export the Express app instance. In production, you would typically start the server separately.
 const express = require("express");
 const app = express();
-app.use(json());
+app.use(express.json());
 
 app.get("/colors", (req, res) => {
-  res.json({ results: ["red", "blue", "green"] });
+  res.json(["red", "blue", "green"]);
 });
 
-export default app;
+module.exports = app;
