6599c08ffa0361f536ee823f369b371fd833056a
karina <karinann514@gmail.com>
2026-04-23 13:38:07 -0400

    Front-end unit test files created

---
+++ __tests__/api.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../app'); // your Express app
+
+test('GET /colors returns 200 and JSON array', async () => {
+  const res = await request(app).get('/colors');
+  expect(res.statusCode).toBe(200);
+  expect(Array.isArray(res.body)).toBe(true);
+});
